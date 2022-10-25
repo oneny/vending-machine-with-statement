ad1177b48bd2975cd01d7521f3abdd702a3c1fd3
oneny <gjwodnjs1996@gmail.com>
2022-10-26 00:30:29 +0900

    Fix: main.js에서 myColaItemList를 MyColaItems로 오타 수정

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ColaItems from "./components/ColaItems.js";
 import CartList from "./components/CartList.js";
-import MyColaItemList from "./components/myColaItemList.js";
+import MyColaItemList from "./components/MyColaItemList.js";
 import MyMoneyBtn from "./components/MyMoneyBtn.js";
 
 const $contList = document.querySelector(".cont-lists");
